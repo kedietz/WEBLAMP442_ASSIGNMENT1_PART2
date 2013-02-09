1fa496a8484676540112e9b0405332f9ef9ed823
Kent Dietz <kent.dietz@outlook.com>
2013-02-09 11:44:35 -0800

    Fix typo

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -10,6 +10,6 @@ error_reporting( E_ALL | E_STRICT );
 ini_set('display_errors','On');
 ini_set('display_startup_errors','On');
 
-// Autload project level bootstrap file
-require_once( __DIR__ . DIRECTORY_SEPARATOR . '../bootstrap.php' );
+// Autoload project level bootstrap file
+require_once( __DIR__ . DIRECTORY_SEPARATOR . '../Bootstrap.php' );
 ?>
